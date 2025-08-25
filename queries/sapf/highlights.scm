f3c76fccb5ee31e9bbcb250f1e816b3549161f4a
Bss <mx.bss@proton.me>
2025-08-25 22:41:31 +0200

    (sapf) basic highlights

---
+++ queries/sapf/highlights.scm
@@ -0,0 +1,20 @@
+(comment) @comment
+
+(number) @number
+(number_fraction) @number
+(hex_number) @number
+
+(string) @string
+
+(operator) @operator
+
+(identifier) @function
+
+[
+  "["
+  "]"
+  "("
+  ")"
+  "{"
+  "}"
+] @punctuation.bracket
